dae6516fc25a78f1f88532e40ca875b120d643dd
Project Collection Build Service (ghyston) <Project Collection Build Service (ghyston)>
2025-10-15 11:29:45 +0000

    Update all non-major dependencies

--- Tools/Audit/Ignored.cs
+++ Tools/Audit/Ignored.cs
@@ -10,17 +10,5 @@ public record IgnoredVulnerability(string PackageName, string PackageVersion, st
 
 public static class Ignored
 {
-    public static ICollection<IgnoredVulnerability> Vulnerabilities =>
-        new List<IgnoredVulnerability>
-        {
-            new(
-                PackageName: "System.Text.Json",
-                PackageVersion: "9.0.7",
-                Cve: "CVE-2024-43485",
-                Reason: "This is a false positive for v9, and we're not using the [ExtensionData] feature which was affected in previous versions anyway",
-                Evidence: "https://github.com/dotnet/announcements/issues/329"
-                    + ", "
-                    + "https://github.com/DependencyTrack/dependency-track/issues/4568"
-            ),
-        }.AsReadOnly();
+    public static ICollection<IgnoredVulnerability> Vulnerabilities => new List<IgnoredVulnerability> { }.AsReadOnly();
 }
